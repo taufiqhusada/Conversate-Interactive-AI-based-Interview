bdd80821a76e90fed5586139fabd49a6944f3644
Taufiq Husada Daryanto <45747001+taufiqhusada@users.noreply.github.com>
2023-12-02 16:45:05 -0500

    Feat/improve question (#9)
    * feat: add dropdown when asking question
    
    * chore: improve question
    
    * fix bug when asking questions
    
    * feat: add option list for comment
    
    * feat: update question dropdowns
    
    * refactor: update dropdown in question

--- src/components/Feedback.vue
+++ src/components/Feedback.vue
@@ -10,7 +10,13 @@
                     <input v-model="endTimeHHMMSS" class="form-control" type="text" placeholder="End Time (hh:mm:ss)">
                 </div>
             </div>
-            <input v-model="annotation" class="form-control form-control mb-3" type="text" placeholder="Comment">
+            <input v-model="annotation" class="form-control form-control mb-3" type="text" placeholder="Comment" list="commentOptions">
+            <datalist id="commentOptions">
+                <option value="I am good at answering the interview question"></option>
+                <option value="I am bad at answering the interview question"></option>
+                <option value="There is some good and bad part when I answer this interview question"></option>
+                <option value="I am not sure about my performance in this part"></option>
+            </datalist>
             <button v-if="!showChatbox" @click="askGPT" class="btn btn-outline-secondary" type="button">Ask
                 Feedback</button>
         </form>
@@ -27,13 +33,23 @@
                             <div class="dot dot-3"></div>
                         </div>
                         <div v-else v-html="message.content"></div>
+
+                        <div v-if="message.dropdownItems">
+                            <div class="dropdown m-2" @click="toggleDropdown">
+                                <button class="btn btn-outline-dark">Select Suggestion</button>
+                                <div class="dropdown-menu" v-show="isDropdownOpen">
+                                    <a v-for="(item, index) in message.dropdownItems" :key="index" class="dropdown-item" @click="selectDropdownItem(item)">{{ item }}</a>
+                                </div>
+                            </div>
+                            <br>
+                            <p>Or you can type in your question</p>
+                        </div>
                     </div>
                 </div>
             </div>
             <div class="input">
                 <input type="text" v-model="question" placeholder="Type your question here!" @keyup.enter="sendMessage" />
-                <i class="fas fa-microphone" @click="toggleRecording"
-                    :class="{ 'recording': isRecording }">&#xf130;</i>
+                <i class="fas fa-microphone" @click="toggleRecording" :class="{ 'recording': isRecording }">&#xf130;</i>
             </div>
         </div>
 
@@ -64,7 +80,8 @@ type SavedData = {
 interface ChatMessage {
     content: string;
     role: 'user' | 'assistant';
-    isTyping: boolean;
+    isTyping?: boolean;
+    dropdownItems?: string[];
 }
 
 interface ChatMessageBackend {
@@ -98,11 +115,12 @@ export default defineComponent({
             concatenatedFilteredTranscript: '',
             savedData: [] as SavedData[], // Array to store saved data
             currentIndex: -1,
-            showChatbox: false,
+            showChatbox: ref(false),
             chatMessages: [] as ChatMessage[], // Define the type for chatMessages
             backendURL: import.meta.env.VITE_BACKEND_URL,
             isRecording: ref<boolean>(false),
-            recognition: null as SpeechRecognition | null, 
+            recognition: null as SpeechRecognition | null,
+            isDropdownOpen: ref(false),
         };
     },
     methods: {
@@ -149,8 +167,8 @@ export default defineComponent({
                     // Update the feedback field with the response from GPT-4
                     this.scrollToBottom();
                     const repliedMessage = response.data.data;
-                    this.chatMessages.push({ content: repliedMessage, role: 'assistant', isTyping: false });
-                    if (this.chatMessages.length == 2) {
+                    this.chatMessages.push({ content: repliedMessage, role: 'assistant', isTyping: false});
+                    if (this.chatMessages.length == 4) {
                         this.chatMessages.push({ content: "Do you want to try saying this part again in a better way? I can give you feedback again based on that", role: 'assistant', isTyping: false });
                     }
                 } else {
@@ -167,15 +185,43 @@ export default defineComponent({
         },
 
         mapChatMessagesToBackendFormat(chatMessages: ChatMessage[]) {
-            const chatMessagesWithoutTyping = chatMessages.map(({ isTyping, ...rest }) => rest);
+            const chatMessagesWithoutTyping = chatMessages.map(({ isTyping,  dropdownItems, ...rest }) => rest);
             return chatMessagesWithoutTyping;
         },
 
+        
         async askGPT() {
             this.showChatbox = true;
 
+            // Send initial message with a dropdown when chatbox is shown
+            this.chatMessages.push({
+                content: "Hi, what can I help you with?",
+                role: 'assistant',
+                isTyping: false,
+            });
+            this.chatMessages.push({
+                content: `Try asking these questions`,
+                role: 'assistant',
+                isTyping: false,
+                dropdownItems: [
+                    'How to improve this part?',
+                    'How is my performance on this part?',
+                    'Could you give me example how to answer this better?',
+                    'Could you give me example how to answer this using STAR method?',
+                    'What is good about this part?',
+                ] as string[]
+            });
         },
 
+        toggleDropdown() {
+            this.isDropdownOpen = !this.isDropdownOpen;
+        },
+
+        selectDropdownItem(selectedValue: string) {
+            this.question = selectedValue;
+
+            this.sendMessage();
+        },
 
         getTranscript(startTime: number, endTime: number): string {
             const filteredTranscript = this.transcript.filter(entry => {
@@ -188,18 +234,16 @@ export default defineComponent({
         },
 
         async saveFormData() {
-            // Construct the data to save to your API
-            const dataToSave = {
-                secondStart: this.convertHHMMSSToSeconds(this.startTimeHHMMSS),
-                secondEnd: this.convertHHMMSSToSeconds(this.endTimeHHMMSS),
-                annotation: this.annotation,
-                feedback: this.feedback,
-                transcript: this.concatenatedFilteredTranscript,
-                question: this.question,
-                chatMessages: this.chatMessages,
-            } as SavedData;
-
             try {
+                const dataToSave = {
+                    secondStart: this.convertHHMMSSToSeconds(this.startTimeHHMMSS),
+                    secondEnd: this.convertHHMMSSToSeconds(this.endTimeHHMMSS),
+                    annotation: this.annotation,
+                    feedback: this.feedback,
+                    transcript: this.concatenatedFilteredTranscript,
+                    question: this.question,
+                    chatMessages: this.chatMessages,
+                } as SavedData;
 
                 if (this.currentIndex >= 0 && this.currentIndex < this.savedData.length) { // update data
                     // Make a PUT request to your update API
@@ -222,6 +266,7 @@ export default defineComponent({
 
                         // Update the currentIndex to the last saved entry
                         this.currentIndex = this.savedData.length;
+                        this.showChatbox = false;
                     } else {
                         console.error('Failed to save data:', response.status, response.data);
                     }
@@ -245,6 +290,7 @@ export default defineComponent({
 
                         // Update the currentIndex to the last saved entry
                         this.currentIndex = this.savedData.length;
+                        this.showChatbox = false;
                     } else {
                         console.error('Failed to save data:', response.status, response.data);
                     }
@@ -260,6 +306,7 @@ export default defineComponent({
                 this.currentIndex--;
                 // Update the form fields with the selected saved data
                 this.loadSavedData();
+                this.showChatbox = true;
             }
         },
         navigateNext() {
@@ -269,6 +316,8 @@ export default defineComponent({
                 // Update the form fields with the selected saved data
                 this.loadSavedData();
             } else {
+                this.showChatbox = false;
+
                 this.startTimeHHMMSS = "";
                 this.endTimeHHMMSS = "";
                 this.annotation = '';
@@ -314,14 +363,14 @@ export default defineComponent({
         toggleRecording() {
             if (this.isRecording) {
                 // Stop recording
-                if (this.recognition){
+                if (this.recognition) {
                     this.recognition.stop();
                     this.recognition.onresult = null; // Remove the onresult event handler
                     this.isRecording = false;
                 }
             } else {
                 // Start recording
-                this.recognition =  new webkitSpeechRecognition;
+                this.recognition = new webkitSpeechRecognition;
                 this.recognition.lang = 'en-US';
 
                 this.recognition.continuous = true
@@ -333,7 +382,7 @@ export default defineComponent({
                         .map(result => result[0])
                         .map(result => result.transcript)
                         .join('')
-                 
+
                     this.question = t;
                 };
 
@@ -365,7 +414,6 @@ export default defineComponent({
 <style scoped>
 .contact {
     position: relative;
-    /* margin-bottom: 1rem; */
     padding-left: 2rem;
     height: 4.5rem;
     display: flex;
@@ -396,8 +444,9 @@ export default defineComponent({
     /* padding: 6rem; */
     background: #F7F7F7;
     /* You can update the background color as needed */
-    flex-shrink: 2;
+    flex-shrink: 10;  
     overflow-y: auto;
+    height: 30rem;
     box-shadow:
         inset 0 2rem 2rem -2rem rgba(0, 0, 0, 0.05),
         inset 0 -2rem 2rem -2rem rgba(0, 0, 0, 0.05);
@@ -532,4 +581,58 @@ input::placeholder {
     100% {
         transform: scale(1);
     }
-}</style>
+}
+
+/* Custom styles for the dropdown */
+.dropdown {
+    position: relative;
+    display: inline-block;
+}
+
+.dropdown-button {
+    background-color: #6c757d;
+    color: #fff;
+    padding: 8px 12px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+}
+
+.dropdown-menu {
+    display: none;
+    position: absolute;
+    background-color: #fff;
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+    z-index: 1;
+    min-width: 160px;
+}
+
+.dropdown-item {
+    padding: 8px 12px;
+    text-decoration: none;
+    display: block;
+    color: #333;
+}
+
+.dropdown-item:hover {
+    background-color: #f8f9fa;
+}
+
+.dropdown:hover .dropdown-menu {
+    display: block;
+}
+
+@keyframes fadeIn {
+            from {
+                opacity: 0;
+            }
+            to {
+                opacity: 1;
+            }
+        }
+
+/* Apply the animation to the chatbox */
+.message {
+    animation: fadeIn 0.5s ease-in-out;
+}
+</style>
