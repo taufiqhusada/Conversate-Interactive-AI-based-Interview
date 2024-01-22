9b9cb3e0c6bd70669f2bddf17cc7d15298bc4502
Taufiq Husada Daryanto <45747001+taufiqhusada@users.noreply.github.com>
2024-01-22 16:44:07 -0500

    chore: reduce questions

--- src/views/VideoRecorder.vue
+++ src/views/VideoRecorder.vue
@@ -118,7 +118,6 @@ export default {
         "How has your previous education and experience prepared you for this job?",
         "What do you consider to be your greatest strength and why?",
         "What do you consider to be your greatest challenge (weakness)? How are you going about improving up on it?",
-        "Describe a time when you used teamwork to achieve a goal. What was your role and the resulting outcome?"
       ];
 
       const concatenatedListQuestion = listQuestions.value.join(',');
